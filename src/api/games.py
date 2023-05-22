18c6bca5697461766c48e8156807ddf85e7c74e1
franktucci <ftucci@calpoly.edu>
2023-05-22 10:08:24 -0700

    Simulate miraculously works

--- src/api/games.py
+++ src/api/games.py
@@ -6,12 +6,16 @@ from sqlalchemy import create_engine
 import os
 import dotenv
 import sqlalchemy
-import dotenv
 from pydantic import BaseModel
 from typing import List
 import random
 
 router = APIRouter()
+
+def calculate_hits(row):
+    return row.single_count + row.double_count + row.triple_count + row.hr_count
+def calculate_at_bats(row):
+    return calculate_hits(row) + row.walk_count + row.strike_out_count + row.hit_by_pitch_count + row.sac_fly_count + row.other_out_count
 
 class EventCodes(Enum):
     SINGLE = 0
@@ -71,47 +75,143 @@ def get_game(game_id: int):
     return {
         "game_id": game.game_id,
         "created_by": game.created_by,
-        "home_team_id": game.home_team_id,  # change these to name in the future
+        "home_team_id": game.home_team_id,
         "away_team_id": game.away_team_id,
         "home_score": game.home_score,
         "away_score": game.away_score
     }
 
+def simulate_inning(inning, player_stats, lineups, orders):
+    half = 0
+    inn_events = []
+    inn_runs_home = 0
+    inn_runs_away = 0
+    inn_order_home = orders[0]
+    inn_order_away = orders[1]
+
+    while half < 2:
+        half_events, half_runs, batting_order = simulate_half(inning, half, player_stats, lineups[half], orders[half])
+        [inn_events.append(event) for event in half_events]
+        if half == 0:
+            inn_runs_home += half_runs
+            inn_order_home = batting_order
+        else:
+            inn_runs_away += half_runs
+            inn_order_away = batting_order
+        half += 1
+    return inn_events, inn_runs_home, inn_runs_away, inn_order_home, inn_order_away
+
+def simulate_half(inning, half, player_stats, lineup, order):
+    outs = 0
+    half_events = []
+    bases = [False, False, False]
+    half_runs = 0
+
+    while outs < 3:
+        if order > 9:
+            order = 1
+        event, out, runs = simulate_event(inning, half, player_stats[lineup[order]], bases)
+        half_events.append(event)
+        outs += out
+        half_runs += runs
+        order += 1
+    return half_events, half_runs, order
+
+def simulate_event(inning, half, player, bases):
+    runs = 0
+    outs = 0
+    at_bats = calculate_at_bats(player)
+
+    if at_bats < 5:
+        single_prp = .20
+        double_prp = single_prp + .05
+        triple_prp = double_prp + .01
+        hr_prp = triple_prp + .01
+        walk_prp = hr_prp + .10
+        strike_out_prp = walk_prp + .50
+        hit_by_prp = strike_out_prp + .01
+        sac_fly_prp = hit_by_prp + .05
+        other_prp = 1
+    else:
+        single_prp = player.single_count / at_bats
+        double_prp = single_prp + (player.double_count / at_bats)
+        triple_prp = double_prp + (player.triple_count / at_bats)
+        hr_prp = triple_prp + (player.hr_count / at_bats)
+        walk_prp = hr_prp + (player.walk_count / at_bats)
+        strike_out_prp = walk_prp + (player.strike_out_count / at_bats)
+        hit_by_prp = strike_out_prp + (player.hit_by_pitch_count / at_bats)
+        sac_fly_prp = hit_by_prp + (player.hit_by_pitch_count / at_bats)
+        other_prp = 1
+
+    rand = random.random()
+    if (0 <= rand < single_prp) or (hr_prp <= rand < walk_prp) or (strike_out_prp <= rand < hit_by_prp):
+        if 0 <= rand < single_prp:
+            event_code = EventCodes.SINGLE.value
+        elif hr_prp <= rand < walk_prp:
+            event_code = EventCodes.WALK.value
+        else:
+            event_code = EventCodes.HIT_PITCH.value
+        bases.insert(0, True)
+        if bases.pop():
+            runs += 1
+    elif single_prp <= rand < double_prp:
+        event_code = EventCodes.DOUBLE.value
+        bases.insert(0, True)
+        bases.insert(0, False)
+        if bases.pop():
+            runs += 1
+        if bases.pop():
+            runs += 1
+    elif double_prp <= rand < triple_prp:
+        event_code = EventCodes.TRIPLE.value
+        bases.insert(0, True)
+        bases.insert(0, False)
+        bases.insert(0, False)
+        if bases.pop():
+            runs += 1
+        if bases.pop():
+            runs += 1
+        if bases.pop():
+            runs += 1
+    elif triple_prp <= rand < hr_prp:
+        event_code = EventCodes.HR.value
+        bases.insert(0, True)
+        bases.insert(0, False)
+        bases.insert(0, False)
+        bases.insert(0, False)
+        if bases.pop():
+            runs += 1
+        if bases.pop():
+            runs += 1
+        if bases.pop():
+            runs += 1
+        if bases.pop():
+            runs += 1
+    else:
+        if walk_prp <= rand < strike_out_prp:
+            event_code = EventCodes.SINGLE.value
+        elif hit_by_prp <= rand < sac_fly_prp:
+            event_code = EventCodes.WALK.value
+        else:
+            event_code = EventCodes.OTHER_OUT.value
+        outs += 1
+
+    event = {
+            'player_id': player.player_id,
+            'inning': inning,
+            'B/T': half,
+            'enum': event_code
+        }
+    return event, outs, runs
+
 class LineupJson(BaseModel):
     team_id: int
     lineup: List[int]
+
 class GameJson(BaseModel):
     created_by: str
     lineup1: LineupJson
     lineup2: LineupJson
-
-def simulate_innings(inning: int):
-        while inning < 10:
-            BT=0
-            home_bat_order = 1
-            away_bat_order = 1
-            simulate_BTs(BT)
-            BT+=1
-            inning+=1
-
-def simulate_BTs(BT: int):
-    while BT < 2:
-        outs = 0
-        bases = [False, False, False]
-        simulate_outs(outs)
-        BT+=1
-
-def simulate_outs(outs: int):
-    while outs < 3:
-            bat_order = home_bat_order if BT == 0 else away_bat_order
-            lineup = game.lineup1.lineup if BT == 0 else game.lineup2.lineup
-
-def simulate_at_bat(batter: str, strikes: int, walks: int):
-    # input: batter, strikes, walks
-    # ouput: get a strike, get a walk, get on base, get
-    # half_of_inning:
-        # state of field whose on base
-    half_of_inning=0
 
 @router.post("/games/", tags=["games"])
 def simulate(game: GameJson):
@@ -132,6 +232,7 @@ def simulate(game: GameJson):
     * `player`: Player name of batter.
     * `happening`: What the player did. Some examples include Walk, Strikeout, Home Run, etc.
     """
+
     if len(game.lineup1.lineup) != 10 or len(game.lineup2.lineup) != 10:
         raise HTTPException(status_code=422, detail="Endpoint was not given 10 players.")
     if game.lineup1.team_id == game.lineup2.team_id:
@@ -143,24 +244,46 @@ def simulate(game: GameJson):
 
     stmt = (
         sqlalchemy.select(
-            db.events.c.player_id,
-            db.events.c.enum,
-            sqlalchemy.func.count()
+            db.players.c.player_id,
+            sqlalchemy.funcfilter(sqlalchemy.func.count(db.events.c.enum), db.events.c.enum == EventCodes.SINGLE.value).label('single_count'),
+            sqlalchemy.funcfilter(sqlalchemy.func.count(db.events.c.enum), db.events.c.enum == EventCodes.DOUBLE.value).label('double_count'),
+            sqlalchemy.funcfilter(sqlalchemy.func.count(db.events.c.enum), db.events.c.enum == EventCodes.TRIPLE.value).label('triple_count'),
+            sqlalchemy.funcfilter(sqlalchemy.func.count(db.events.c.enum), db.events.c.enum == EventCodes.HR.value).label('hr_count'),
+            sqlalchemy.funcfilter(sqlalchemy.func.count(db.events.c.enum), db.events.c.enum == EventCodes.WALK.value).label('walk_count'),
+            sqlalchemy.funcfilter(sqlalchemy.func.count(db.events.c.enum), db.events.c.enum == EventCodes.STRIKE_OUT.value).label('strike_out_count'),
+            sqlalchemy.funcfilter(sqlalchemy.func.count(db.events.c.enum), db.events.c.enum == EventCodes.HIT_PITCH.value).label('hit_by_pitch_count'),
+            sqlalchemy.funcfilter(sqlalchemy.func.count(db.events.c.enum), db.events.c.enum == EventCodes.SAC_FLY.value).label('sac_fly_count'),
+            sqlalchemy.funcfilter(sqlalchemy.func.count(db.events.c.enum), db.events.c.enum == EventCodes.OTHER_OUT.value).label('other_out_count'),
+            sqlalchemy.funcfilter(sqlalchemy.func.count(db.events.c.enum), db.events.c.enum == EventCodes.STOLEN.value).label('stolen_count'),
+            sqlalchemy.funcfilter(sqlalchemy.func.count(db.events.c.enum), db.events.c.enum == EventCodes.CAUGHT_STEALING.value).label('caught_stealing_count')
         )
-        .where(db.events.c.player_id.in_(game.lineup1.lineup) | db.events.c.player_id.in_(game.lineup2.lineup))
-        .group_by(db.events.c.player_id, db.events.c.enum)
+        .select_from(db.players.join(db.events, db.events.c.player_id == db.players.c.player_id, isouter=True))
+        .where(db.players.c.player_id.in_(game.lineup1.lineup) | db.players.c.player_id.in_(game.lineup2.lineup))
+        .group_by(db.players.c.player_id, db.events.c.enum)
     )
 
     with db.engine.connect() as conn:
         events_result = conn.execute(stmt)
 
-    stats = {}
+    player_stats = {
+        player.player_id: player
+        for player in events_result
+    }
 
-    for row in events_result:
-        if stats.get(row[0]) is None:
-            stats[row[0]] = {row[1]: row[2]}
-        else:
-            stats[row[0]][row[1]] = row[2]
+    inning = 1
+    home_score = 0
+    away_score = 0
+    events = []
+    lineups = [game.lineup1.lineup, game.lineup2.lineup]
+    orders = [1, 1]
+
+    while inning < 10:
+        inn_events, inn_runs_home, inn_runs_away, inn_order_home, inn_order_away = simulate_inning(inning, player_stats, lineups, orders)
+        [events.append(event) for event in inn_events]
+        home_score += inn_runs_home
+        away_score += inn_runs_away
+        orders = [inn_order_away, inn_order_away]
+        inning += 1
 
     stmt = (sqlalchemy.select(db.games.c.game_id).order_by(sqlalchemy.desc('game_id')))
     with db.engine.connect() as conn:
@@ -172,166 +295,6 @@ def simulate(game: GameJson):
         event_result = conn.execute(stmt)
     event_id = event_result.first().event_id + 1
 
-    events = []
-
-    inning = 1
-    home_score = 0
-    away_score = 0
-    while inning < 10:
-        BT = 0
-        home_bat_order = 1
-        away_bat_order = 1
-        while BT < 2:
-            outs = 0
-            bases = [False, False, False]
-            while outs < 3:
-                bat_order = home_bat_order if BT == 0 else away_bat_order
-                lineup = game.lineup1.lineup if BT == 0 else game.lineup2.lineup
-
-                if stats.get(lineup[bat_order]) is None:
-                    stats[lineup[bat_order]] = {}
-
-                singles = stats[lineup[bat_order]].get(EventCodes.SINGLE.value) or 0
-                doubles = stats[lineup[bat_order]].get(EventCodes.DOUBLE.value) or 0
-                triples = stats[lineup[bat_order]].get(EventCodes.TRIPLE.value) or 0
-                hrs = stats[lineup[bat_order]].get(EventCodes.HR.value) or 0
-                walks = stats[lineup[bat_order]].get(EventCodes.WALK.value) or 0
-                strike_outs = stats[lineup[bat_order]].get(EventCodes.STRIKE_OUT.value) or 0
-                hit_bys = stats[lineup[bat_order]].get(EventCodes.HIT_PITCH.value) or 0
-                sac_flies = stats[lineup[bat_order]].get(EventCodes.SAC_FLY.value) or 0
-                other_outs = stats[lineup[bat_order]].get(EventCodes.OTHER_OUT.value) or 0
-
-                # We'll use these later
-                stolen = stats[lineup[bat_order]].get(EventCodes.STOLEN.value) or 0
-                caught_stealing = stats[lineup[bat_order]].get(EventCodes.CAUGHT_STEALING.value) or 0
-
-                hits = singles + doubles + triples + hrs
-                at_bats = hits + walks + strike_outs + hit_bys + sac_flies + other_outs
-
-                if at_bats < 5:
-                    single_prp = .20
-                    double_prp = single_prp + .05
-                    triple_prp = double_prp + .01
-                    hr_prp = triple_prp + .01
-                    walk_prp = hr_prp + .10
-                    strike_out_prp = walk_prp + .50
-                    hit_by_prp = strike_out_prp + .01
-                    sac_fly_prp = hit_by_prp + .05
-                    other_prp = 1
-                else:
-                    single_prp = singles / at_bats
-                    double_prp = single_prp + (doubles / at_bats)
-                    triple_prp = double_prp + (triples / at_bats)
-                    hr_prp = triple_prp + (hrs / at_bats)
-                    walk_prp = hr_prp + (walks / at_bats)
-                    strike_out_prp = walk_prp + (strike_outs / at_bats)
-                    hit_by_prp = strike_out_prp + (hit_bys / at_bats)
-                    sac_fly_prp = hit_by_prp + (sac_flies / at_bats)
-                    other_prp = 1
-
-                rand = random.random()
-                if (0 <= rand < single_prp) or (hr_prp <= rand < walk_prp) or (strike_out_prp <= rand < hit_by_prp):
-                    if 0 <= rand < single_prp:
-                        event_code = EventCodes.SINGLE.value
-                    elif hr_prp <= rand < walk_prp:
-                        event_code = EventCodes.WALK.value
-                    else:
-                        event_code = EventCodes.HIT_PITCH.value
-                    bases.insert(0, True)
-                    if bases.pop():
-                        if BT == 0:
-                            home_score += 1
-                        else:
-                            away_score += 1
-                elif single_prp <= rand < double_prp:
-                    event_code = EventCodes.DOUBLE.value
-                    bases.insert(0, True)
-                    bases.insert(0, False)
-                    if bases.pop():
-                        if BT == 0:
-                            home_score += 1
-                        else:
-                            away_score += 1
-                    if bases.pop():
-                        if BT == 0:
-                            home_score += 1
-                        else:
-                            away_score += 1
-                elif double_prp <= rand < triple_prp:
-                    event_code = EventCodes.TRIPLE.value
-                    bases.insert(0, True)
-                    bases.insert(0, False)
-                    bases.insert(0, False)
-                    if bases.pop():
-                        if BT == 0:
-                            home_score += 1
-                        else:
-                            away_score += 1
-                    if bases.pop():
-                        if BT == 0:
-                            home_score += 1
-                        else:
-                            away_score += 1
-                    if bases.pop():
-                        if BT == 0:
-                            home_score += 1
-                        else:
-                            away_score += 1
-                elif triple_prp <= rand < hr_prp:
-                    event_code = EventCodes.HR.value
-                    bases.insert(0, True)
-                    bases.insert(0, False)
-                    bases.insert(0, False)
-                    bases.insert(0, False)
-                    if bases.pop():
-                        if BT == 0:
-                            home_score += 1
-                        else:
-                            away_score += 1
-                    if bases.pop():
-                        if BT == 0:
-                            home_score += 1
-                        else:
-                            away_score += 1
-                    if bases.pop():
-                        if BT == 0:
-                            home_score += 1
-                        else:
-                            away_score += 1
-                    if bases.pop():
-                        if BT == 0:
-                            home_score += 1
-                        else:
-                            away_score += 1
-                else:
-                    if walk_prp <= rand < strike_out_prp:
-                        event_code = EventCodes.SINGLE.value
-                    elif hit_by_prp <= rand < sac_fly_prp:
-                        event_code = EventCodes.WALK.value
-                    else:
-                        event_code = EventCodes.OTHER_OUT.value
-                    outs += 1
-                events.append(
-                    {
-                        'event_id': event_id,
-                        'game_id': game_id,
-                        'player_id': lineup[bat_order],
-                        'inning': inning,
-                        'B/T': BT,
-                        'enum': event_code
-                    }
-                )
-                event_id += 1
-                if BT == 0:
-                    home_bat_order += 1
-                    if home_bat_order > 9:
-                        home_bat_order = 1
-                else:
-                    away_bat_order += 1
-                    if away_bat_order > 9:
-                        away_bat_order = 1
-            BT += 1
-        inning += 1
     with db.engine.begin() as conn:
         conn.execute(
             db.games.insert().values(
@@ -346,7 +309,7 @@ def simulate(game: GameJson):
         for event in events:
             conn.execute(
                 db.events.insert().values(
-                    event_id=event['event_id'],
+                    event_id=event_id,
                     game_id=game_id,
                     player_id=event['player_id'],
                     inning=event['inning'],
@@ -354,4 +317,5 @@ def simulate(game: GameJson):
                     enum=event['enum']
                 )
             )
+            event_id += 1
     return {'game_id': game_id}
